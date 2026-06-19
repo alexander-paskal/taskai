29185155e753736bebdb6b60c2a10792146da1f6
Alex Paskal <alexcpaskal@gmail.com>
2026-06-19 13:57:29 -0700

    working db with tests

--- taskai/json_dir_database.py
+++ taskai/json_dir_database.py
@@ -2,6 +2,9 @@
 import os
 from pathlib import Path
 import shutil
+from copy import copy
+import dataclasses
+from rich import print
 
 # local
 from taskai.models import *
@@ -9,18 +12,29 @@ from taskai.models import *
 # external
 import orjson as json
 
-class JsonDirectoryDatabase:
 
+class DatabaseError(Exception):
+    pass
+
+
+class JsonDirectoryDatabase:
+    """
+    All views extracted from the database are read only
+    All updates should go through the database api
+    The database is reponsible for ensuring a valid data model on every write
+    """
     def __init__(
             self,
             dirpath: os.PathLike,
-            user: str
+            user: str,
+            debug: bool = False
     ):
         
-        self.db_dir = Path(dirpath)
-        self.user_name = user
-        self.user_data_path = Path(dirpath) / (user.strip(" ") + ".json")
-        self.user_data = None
+        self.db_dir: Path = Path(dirpath)
+        self.user_name: str = user
+        self.user_data_path: Path = Path(dirpath) / (user.strip(" ") + ".json")
+        self.user_data: UserData = None
+        self.debug = debug
 
         if not os.path.exists(self.db_dir):
             self._setup_directory()
@@ -28,58 +42,15 @@ class JsonDirectoryDatabase:
         if not os.path.exists(self.user_data_path):
             self._setup_user_data()
     
-    def remove(self):
-        if os.path.exists(self.db_dir):
-            shutil.rmtree(self.db_dir)
-
-    # setup utils
-    def _setup_directory(self):
-        os.makedirs(self.db_dir, exist_ok=True)
-
-    def _setup_user_data(self):
-        user_data = User(
-            id="0",
-            name=self.user_name
-        ).model_dump()
-
-        user_data[TodoList.__name__] = {}
-        user_data[TodoItem.__name__] = {}
-        user_data[Comment.__name__] = {}
-        user_data["config"] = {}
-
-        self.user_data = user_data
-        self.commit()
-
-    # data accessors
-    @property
-    def lists(self) -> dict:
-        return self.user_data[TodoList.__name__]    
-    
-    @property
-    def items(self) -> dict:
-        return self.user_data[TodoItem.__name__]
-
-    @property
-    def comments(self) -> dict:
-        return self.user_data[Comment.__name__]
-    
-    @property
-    def user_id(self):
-        return self.user_data["id"]
-    
-    @property
-    def config(self) -> dict:
-        return self.user_data["config"]
-
-    # io
+    # IO
     def connect(self):
         with open(self.user_data_path, "rb") as f:
             json_bstring = f.read()
-            self.user_data = json.loads(json_bstring)
+            self.user_data = UserData(**json.loads(json_bstring))
 
     def commit(self):
         with open(self.user_data_path, "wb") as f:
-            json_bstring = json.dumps(self.user_data)
+            json_bstring = json.dumps(self.user_data.model_dump())
             f.write(json_bstring)
     
     def flush(self):
@@ -88,117 +59,213 @@ class JsonDirectoryDatabase:
     def close(self):
         self.flush()
     
-    # crud
-    def create(self, record: Base) -> int:
+    def remove(self):
+        if os.path.exists(self.db_dir):
+            shutil.rmtree(self.db_dir)
 
-        record.id = str(self.user_data["id_counter"])
-        record.user_id = self.user_id
-
-        if isinstance(record, TodoList):
-            self.lists[record.id] = record.model_dump()
-
-        elif isinstance(record, TodoItem):
-            self.items[record.id] = record.model_dump()
-            self.lists[record.list_id]["item_ids"].append(record.id)
-        
-        elif isinstance(record, Comment):
-            self.comments[record.id] = record.model_dump()
-            self.items[record.item_id]["comment_ids"].append(record.id)
-        else:
-            raise RuntimeError("don't know what you're creating")
-        
-        self.user_data["id_counter"] += 1
-        return record.id
+    # CRUD
+    def get_item(self, id: int) -> TodoItem:
+        try:
+            return TodoItem(**self.user_data.todo_items[id])
+        except KeyError:
+            raise DatabaseError(f"No record by id {id}")
     
-    def read(self, id_: int|str) -> Base:
-        id_ = str(id_)
-        if id_ in self.user_data[TodoList.__name__]:
-            return TodoList(**self.lists[id_])
-        elif id_ in self.user_data[TodoItem.__name__]:
-            return TodoItem(**self.items[id_])
-        elif id_ in self.user_data[Comment.__name__]:
-            return Comment(**self.comments[id_])
-        else:
-            raise RuntimeError("Can't find record to read")
+    def get_items(self, ids: list[int]) -> list[TodoItem]:
+        return [self.get_item(id) for id in ids]
+
+    def get_item_attr(self, id: int, key: str) -> any:
+        try:
+            return copy(self.user_data.todo_items[id][key]) 
+        except KeyError:
+            raise DatabaseError(f"No record by id {id}")
     
-    def read_batch_attr(self, record_type: type[Base], attr: str, keep_nulls=False) -> dict[id, str]:
-        """Returns a dictonary of attr values for a given attribute name"""
-        record_store = {
-            TodoItem: self.items,
-            TodoList: self.lists,
-            Comment: self.comments
-        }[record_type]
-
-        # TODO make sure attr is on the model type else throw error
+    def get_comment(self, id: int) -> Comment:
+        try:
+            return Comment(**self.user_data.comments[id])
+        except KeyError:
+            raise DatabaseError(f"No record by id {id}")
+    
+    def get_config(self) -> CLIConfig:
+        return CLIConfig(**self.user_data.config)
+    
+    def create_item(self, name: str, **kwargs) -> int:
+        item = TodoItem(name=name, **kwargs)  # validate
+        item.id = self._get_new_id()
         
-        records = {
-            id_: record[attr] for id_, record in record_store.items() if record.get(attr) or keep_nulls
-        }
-        return records
+        # create item
+        self._debug(f"Creating item {item.id}")
+        self.user_data.todo_items[item.id] = item.model_dump()
 
-    def update(self, record: Base) -> None:
+        # update parent
+        if kwargs.get("parent_id"):
+            self._validate_parentage(kwargs["parent_id"], item.id)
+
+        return item.id
+        
+    def create_comment(self, content: str, item_id: int, **kwargs) -> int:
+        comment = Comment(content, item_id=item_id, **kwargs)
+        comment.id = self._get_new_id()
+        
+        # create comment
+        self._debug(f"Creating comment {comment.id}")
+        self.user_data.comments[comment.id] = comment.model_dump()
+
+        # update item
+        self._validate_comment(comment.id, comment.item_id)
+
+        return comment.id
+        
+    def delete_item(self, id: int) -> bool:
+        if id not in self.user_data.todo_items:
+            raise DatabaseError(f"No record by id {id}")
+        parent_id = self.get_item_attr(id, "parent_id")
+        self._debug(f"Deleting item {id}")
+        item_dict = self.user_data.todo_items.pop(id)
+        self._validate_parentage(parent_id, id)
+        # recursively delete children
+        for child_id in item_dict["child_ids"]:
+            self.delete_item(child_id)
+
+    def delete_comment(self, id: int) -> bool:
+        if id not in self.user_data.comments:
+            raise DatabaseError(f"No record by id {id}")
+        comment = self.get_comment(id)
+        self._debug(f"Deleting comment {id}")
+        self.user_data.comments.pop(id)
+        self._validate_parentage(comment.id, comment.item_id)    
+    
+    def update_item(self, id: int, **kwargs) -> bool:
+        if id not in self.user_data.todo_items:
+            raise DatabaseError(f"No record by id {id}")
+        # validate the full new item
+        item_dict = self.user_data.todo_items[id].copy()
+        item_dict.update(kwargs)
+        new_item_dict = TodoItem(**item_dict).model_dump()
+
+        if new_item_dict["parent_id"]:
+            self._validate_parentage(new_item_dict["parent_id"], id)
+
+        self._debug(f"Updating item {id}")
+        self.user_data.todo_items[id] = new_item_dict
+
+    def update_comment(self, id: int, **kwargs) -> bool:
+        if id not in self.user_data.comments:
+            raise DatabaseError(f"No record by id {id}")
+        # validate the full new item
+        comment_dict = self.user_data.todo_items[id].copy()
+        comment_dict.update(kwargs)
+        new_comment_dict = Comment(**comment_dict).model_dump()
+        self._validate_comment(id, new_comment_dict["child_id"])
+        self._debug(f"Updating comments {id}")
+        self.user_data.comments[id] = new_comment_dict
+
+    def update_config(self, **kwargs) -> bool:
+        config_dict = self.user_data.config.copy()
+        config_dict.update(kwargs)
+        self._debug(f"Updating config {id}")
+        new_config_dict = CLIConfig(**config_dict).model_dump()
+        self.user_data.config = new_config_dict
+
+    def _validate_comment(self, comment_id: int, item_id: int):
+        item_exists = item_id in self.user_data.todo_items
+        comment_exists = comment_id in self.user_data.comments
+
+        match (item_exists, comment_exists):
+            case (True, True):  # make sure they reference each other
+                comment = self.get_comment(comment_id)
+                comment_ids = self.get_item_attr(item_id, "comment_ids")
+
+                # make sure the item references the comment in its comment list
+                if comment_id not in comment_ids:
+                    self._debug(f"Adding comment {comment_id} to item {item_id}")
+                    comment_ids.append(comment_id)
+                    self.update_item(item_id, comment_ids=comment_ids)
+
+                # make sure the comment references its parent
+                if comment.item_id != item_id:
+                    self._debug(f"Setting comment {comment_id} to reference item {item_id}")
+                    self.update_item(comment_id, item_id=item_id)
             
-        if isinstance(record, TodoItem):
-
-            assert record.id in self.items, "item doesn't exist"
+            case (False, True):  # delete the comment if its parent is deceesed
+                self._debug(f"Deleting orphan comment {comment_id}")
+                self.user_data.comments.pop(comment_id)
             
-            # update dependencies
-            old_item = self.items[record.id]
-            if old_item["list_id"] != record.list_id:
-                old_list = self.lists[old_item["list_id"]]
-                old_list["item_ids"].remove(record.id)
-                new_list = self.lists[record.list_id]
-                new_list["item_ids"].append(record.id)
+            case (True, False):  # remove the comment id from the item's list of comments
+                comment_ids =  self.get_item_attr(item_id, "comment_ids")
+                if comment_id in comment_ids:
+                    self._debug(f"Removing reference to comment {comment_id} from item {item_id}")
+                    comment_ids.remove(comment_id)
+                    self.update_item(item_id, child_ids=comment_ids)
+            
+            case (False, False):
+                self._debug(f"Couldn't find reference to comment {comment_id} or item {item_id}")    
 
-            self.items[record.id] = record.model_dump()
-        
-        elif isinstance(record, TodoList):
-            assert record.id in self.lists, "list doesn't exist"
-            old_list: dict = self.lists[record.id]
-            for item_id in old_list["item_ids"]:
+    def _validate_parentage(self, parent_id: int, child_id: int):
+        parent_exists = parent_id in self.user_data.todo_items
+        child_exists = child_id in self.user_data.todo_items
 
-                # Remove any items that have been dropped from the list that still hold it as their list id,
-                # as they are now orphans
-                if self.items[item_id]["list_id"] == old_list["id"] and item_id not in record.item_ids:
-                    self.items.pop(item_id)
-            self.lists[record.id] = record.model_dump()
+        match (parent_exists, child_exists):
+            case (True, True):  # make sure they reference each other
+                child = self.get_item(child_id)
+                parent_children_ids = self.get_item_attr(parent_id, "child_ids")
+                
+                # make sure the child id is in the parent's children list
+                if child_id not in parent_children_ids:
+                    self._debug(f"Adding child {child_id} to parent {parent_id}")
+                    parent_children_ids.append(child_id)
+                    self.update_item(parent_id, child_ids=parent_children_ids)
 
-        else:
-            raise RuntimeError("don't know what you're updating")
-        
-    def delete(self, id_: int|str) -> None:
-        id_ = str(id_)
-        if id_ in self.user_data[TodoList.__name__]:
-            list_: TodoList = TodoList(**self.lists.pop(id_))
-            for item_id in list_.item_ids:
-                self.items.pop(item_id)
-        elif id_ in self.user_data[TodoItem.__name__]:
-            item: TodoItem = TodoItem(**self.items.pop(id_))
-            self.lists[item.list_id]["item_ids"].remove(id_)
-        elif id_ in self.user_data[Comment.__name__]:
-            comment: Comment = Comment(**self.comments.pop(id_))
-            self.items[comment.item_id].comment_ids.remove(id_)
-        else:
-            raise RuntimeError("Don't know what you're deleting")
-    
-
-    # Utilities
-    def get_list_by_name(self, name: str):
-        for list_id in self.lists:
-            list_: TodoList = self.read(list_id)
-            if list_.name.lower() == name.lower():
-                return list_
-        raise RuntimeError("Don't recognize list")
-
-
-    # Config
-    def get_config_value(self, key: str) -> any:
-        return self.user_data["config"].get(key)
+                # make sure the child references the parent
+                if child.parent_id != parent_id:
+                    self._debug(f"Setting child {child_id} to reference parent {parent_id}")
+                    self.update_item(child_id, parent_id=parent_id)
+            
+            case (False, True):  # set child parent_id to null
+                self._debug(f"Setting child {child_id} as root")
+                self.update_item(child_id, parent_id=None)
+            
+            case (True, False):  # remove the child id from the parents list
+                parent_children_ids=  self.get_item_attr(parent_id, "child_ids")
+                if child_id in parent_children_ids:
+                    self._debug(f"Removing reference to child {child_id} from parent {parent_id}")
+                    parent_children_ids.remove(child_id)
+                    self.update_item(parent_id, child_ids=parent_children_ids)
+            
+            case (False, False):
+                self._debug(f"Couldn't find reference to child {child_id} or parent {parent_id}")
+                
 
     def get_config(self) -> CLIConfig:
         return CLIConfig(**self.user_data.get("config", {}))
+    
+    # UTILS
+    def _setup_directory(self):
+        os.makedirs(self.db_dir, exist_ok=True)
 
-    def set_config_value(self, key: str, value: any):
-        if key not in CLIConfig.model_fields:
-            raise RuntimeError("Invalid Config Option")
-        self.user_data["config"][key] = value
+    def _setup_user_data(self):
+        user_data = UserData(
+            id="0",
+            name=self.user_name
+        )
+        self.user_data = user_data
+        self.commit()
+    
+    def _get_new_id(self) -> int:
+        self.user_data.id_counter += 1
+        return self.user_data.id_counter
+
+    def _debug(self, *args, **kwargs):
+        if self.debug:
+            print(*args, **kwargs)
+
+    def _debug_print_tree(self):
+
+        def _recursive_print(item, level):
+            print("\t"*level + f"{item.id}-{item.name}" )
+            for child_id in item.child_ids:
+                child = self.get_item(child_id)
+                _recursive_print(child, level+1)
+        
+        roots = [child for child in self.get_items(self.user_data.todo_items.keys()) if not child.parent_id]
+        for root in roots:
+            _recursive_print(root, 0)
